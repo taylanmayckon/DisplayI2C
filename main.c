0605ea3564d3f541145703b3dea0101924380920
Taylan <eusoutaylann@gmail.com>
2025-02-08 11:52:36 -0300

    Add: Interrupção botões A e B com funcionalidades de LEDS e Serial

--- main.c
+++ main.c
@@ -1,57 +1,90 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-#include "hardware/uart.h"
+#include "libs/led_matrix.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+// Definindo os pinos do I2C
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+// Definindo o endereço do display 
+#define DISPLAY_ADRESS 0x3C
+// Definição dos botões
+#define BUTTON_A 5
+#define BUTTON_B 6
+// Definição dos LEDs RGB
+#define LED_RED 13
+#define LED_GREEN 11
+#define LED_BLUE 12
+// Definição da máscara para ativar os LEDs RGB
+#define OUTPUT_MASK ((1 << LED_RED) | (1 << LED_GREEN) | (1 << LED_BLUE))
+// Máscara para GPIO dos botões
+#define INPUT_MASK ((1 << BUTTON_A) | (1 << BUTTON_B))
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
+// Variável que armazena o tempo do último evento (em us)
+static volatile uint32_t last_time = 0;
 
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
+// Função de interrupção da GPIO
+void gpio_irq_handler(uint gpio, uint32_t events){
+    // Obtendo tempo atual (em us)
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    // Aplicação do debounce
+    if(current_time - last_time > 200000){
+        last_time = current_time;
+
+        if(gpio == BUTTON_A){
+            gpio_put(LED_GREEN, !gpio_get(LED_GREEN));
+            // Registro da ação
+            if(gpio_get(LED_GREEN)){
+                printf("LED GREEN: ON\n");
+            }
+            else{
+                printf("LED_GREEN: OFF\n");
+            }
+        }
+
+        if(gpio == BUTTON_B){
+            gpio_put(LED_BLUE, !gpio_get(LED_BLUE));
+            // Registro da ação
+            if(gpio_get(LED_BLUE)){
+                printf("LED BLUE: ON\n");
+            }
+            else{
+                printf("LED BLUE: OFF\n");
+            }
+        }
+    }
+}
 
 
-
-int main()
-{
+int main(){
     stdio_init_all();
 
-    // I2C Initialisation. Using it at 400Khz.
+    // Inicializando o I2C com 400KHz
     i2c_init(I2C_PORT, 400*1000);
-    
+    // Setando as funções dos pinos do I2C
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    // Garantindo o Pull up do I2C
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+    // Inicializando todos os pinos da máscara de OUTPUT
+    gpio_init_mask(OUTPUT_MASK);
+    // Definindo como saída
+    gpio_set_dir_out_masked(OUTPUT_MASK);
+
+    // Inicializando todos os pinos da máscara de INPUT
+    gpio_init_mask(INPUT_MASK);
+    // Definindo os botões como pull up
+    gpio_pull_up(BUTTON_A);
+    gpio_pull_up(BUTTON_B);
+    // Configurando as interupções dos botões
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        sleep_ms(10); // Delay para reduzir o consumo de CPU
     }
 }
