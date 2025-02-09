85beae4c23e26009781949d520bf2387581c4499
Taylan <eusoutaylann@gmail.com>
2025-02-09 02:59:05 -0300

    Add: Lógica de transição ON/OFF dos leds no display

--- main.c
+++ main.c
@@ -22,12 +22,51 @@
 #define LED_MATRIX_PIN 7
 #define IS_RGBW false
 
+ssd1306_t ssd; // Inicializa a estrutura do display no escopo global
+
 // Variável que armazena o tempo do último evento (em us)
 static volatile uint32_t last_time = 0;
+static volatile bool green_state = false;
+static volatile bool blue_state = false;
 
 // Variáveis da PIO declaradas no escopo global
 PIO pio;
 uint sm;
+
+// Função que imprime o estado dos leds no display
+void led_state_on_display(char *led_selected, int x_pos, int y_pos, bool bool_state){
+    char *char_state;
+    char *erase_string = "    "; // string para limpar o ON/OFF presente no display
+    int x_offset;
+    int y_offset = 10;
+
+    if(led_selected == "GREEN"){
+        x_offset = 8;
+    }
+    else{
+        x_offset = 4;
+    }
+
+    /*
+        ssd1306_draw_string(&ssd, "GREEN", 12, 42); // Verde
+        ssd1306_draw_string(&ssd, "OFF", 20, 52);  // Verde
+        ssd1306_draw_string(&ssd, "BLUE", 80, 42); // Azul
+        ssd1306_draw_string(&ssd, "OFF", 84, 52);  // Azul
+    */
+
+    if(bool_state){
+        char_state = "ON";
+        x_offset += 4;
+    }
+    else{
+        char_state = "OFF";
+    }
+
+    ssd1306_draw_string(&ssd, erase_string, x_pos, y_pos + y_offset); // Limpando antes de imprimir no display
+
+    ssd1306_draw_string(&ssd, led_selected, x_pos, y_pos); // Verde
+    ssd1306_draw_string(&ssd, char_state, x_pos + x_offset, y_pos + y_offset);
+}
 
 // Função de interrupção da GPIO
 void gpio_irq_handler(uint gpio, uint32_t events){
@@ -47,6 +86,8 @@ void gpio_irq_handler(uint gpio, uint32_t events){
             else{
                 printf("LED GREEN: OFF\n");
             }
+            green_state = gpio_get(LED_GREEN); // Atualiza o booleano da impressão no display
+            led_state_on_display("GREEN", 12, 42, green_state); // Escreve ON/OFF no display
         }
 
         if(gpio == BUTTON_B){
@@ -58,10 +99,13 @@ void gpio_irq_handler(uint gpio, uint32_t events){
             else{
                 printf("LED BLUE: OFF\n");
             }
+            blue_state = gpio_get(LED_BLUE); // Atualiza o booleano da impressão no display
+            led_state_on_display("BLUE", 80, 42, blue_state);
         }
+
+        ssd1306_send_data(&ssd); // Atualiza o display
     }
 }
-
 
 int main(){
     stdio_init_all();
@@ -75,7 +119,6 @@ int main(){
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
     // Configuração do display
-    ssd1306_t ssd; // Inicializa a estrutura do display
     ssd1306_init(&ssd, WIDTH, HEIGHT, false, DISPLAY_ADRESS, I2C_PORT); // Inicializa o display
     ssd1306_config(&ssd); // Configura o display
     ssd1306_send_data(&ssd); // Envia os dados para o display
@@ -103,12 +146,19 @@ int main(){
     uint offset = pio_add_program(pio, &ws2812_program);
     ws2812_program_init(pio, sm, offset, LED_MATRIX_PIN, 800000, IS_RGBW);
 
+    // Inicializa o estado dos LEDs como off no display
+    led_state_on_display("GREEN", 12, 42, green_state);
+    led_state_on_display("BLUE", 80, 42, green_state);
+
     while (true) {
         // Atualização do display
         ssd1306_rect(&ssd, 0, 0, 127, 63, cor, !cor); // Desenha um retângulo
-        ssd1306_draw_string(&ssd, "TAYLAN MAYCKON", 8, 10); // Desenha uma string
-        ssd1306_draw_string(&ssd, "Caatronics", 20, 30); // Desenha uma string
-        ssd1306_draw_string(&ssd, "Zeri", 15, 48); // Desenha uma string      
+        ssd1306_draw_string(&ssd, "TAYLAN MAYCKON", 8, 8); // Desenha uma string
+        ssd1306_draw_string(&ssd, "CAATRONICS", 8, 20); // Desenha uma string
+
+        // Estado dos LEDs
+        ssd1306_rect(&ssd, 36, 0, 127, 2, cor, cor); // Divisória entre as entradas e leds
+        ssd1306_rect(&ssd, 38, 63, 2, 25, cor, cor); // Divisória entre os leds
         ssd1306_send_data(&ssd); // Atualiza o display
 
         // Recebimento dos caracteres via UART
@@ -118,6 +168,6 @@ int main(){
         // Atualiza MATRIZ DE LEDS, se for entrada numérica
         atualiza_numero(entrada);
         
-        sleep_ms(1000); // Delay para reduzir o consumo de CPU
+        sleep_ms(10); // Delay para reduzir o consumo de CPU
     }
 }
